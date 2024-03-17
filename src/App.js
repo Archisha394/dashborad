f1c6dc8613be9e31eb37847ab37c5bdf7d4adf77
Abhijeet Nigam <basant.abhijeet2003@gmail.com>
2024-03-17 19:49:06 +0530

    States Done by Abhijeet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import logo from "./logo.png";
 import "./App.css";
 import map from "./assets/Map.png";
@@ -16,9 +16,29 @@ import sexeductn from "./assets/sex-education 1.png";
 import India from "./assets/INDIA.png";
 import Input from "./Inputs";
 import ImageMapAreas from "./comp/ImageMapAreas";
-import MaharashtraImage from './assets/stateImages/Maharashtra.png'
-import JnKImage from './assets/stateImages/JnK.png'
-hello
+import MaharashtraImage from "./assets/stateImages/Maharashtra.png";
+import APImage from "./assets/stateImages/AndhraPradesh.png";
+import AssamImage from "./assets/stateImages/Assam.png";
+import BiharImage from "./assets/stateImages/Bihar.png";
+import ChattisgarhImage from "./assets/stateImages/Chattisgarh.png";
+import DelhiImage from "./assets/stateImages/Delhi.png";
+import GujaratImage from "./assets/stateImages/Gujarat.png";
+import HaryanaImage from "./assets/stateImages/Haryana.png";
+import HPImage from "./assets/stateImages/HimachalPradesh.png";
+import JharkhandImage from "./assets/stateImages/Jharakhand.png";
+import KarnatakaImage from "./assets/stateImages/Karnataka.png";
+import MPImage from "./assets/stateImages/MP.png";
+import ManipurImage from "./assets/stateImages/Manipur.png";
+import MeghalayaImage from "./assets/stateImages/Meghalaya.png";
+import NagalandImage from "./assets/stateImages/Nagaland.png";
+import OrrisaImage from "./assets/stateImages/Orrisa.png";
+import PunjabImage from "./assets/stateImages/Punjab.png";
+import RajasthanImage from "./assets/stateImages/Rajasthan.png";
+import TNImage from "./assets/stateImages/TamilNadu.png";
+import TelanganaImage from "./assets/stateImages/Telangana.png";
+import UpImage from "./assets/stateImages/UP.png";
+import WestBengalImage from "./assets/stateImages/WestBengal.png";
+import KeralaImage from "./assets/stateImages/Kerala.png";
 
 function App() {
   const [currentStateImage, setCurrentStateImage] = useState(null);
@@ -26,6 +46,28 @@ function App() {
   const stateImages = {
     Maharashtra: MaharashtraImage,
     JnK: JnKImage,
+    Telangana: TelanganaImage,
+    // AndhraPradesh: ,
+    TamilNadu: TNImage,
+    Kerala: KeralaImage,
+    Orissa: OrrisaImage,
+    Jharkhand: JharkhandImage,
+    UttarPradesh: UpImage,
+    Punjab: PunjabImage,
+    HimachalPradesh: HPImage,
+    // Uttarakhand: utt
+    Bihar: BiharImage,
+    Haryana: HaryanaImage,
+    WestBengal: WestBengalImage,
+    Chattisgarh: ChattisgarhImage,
+    Gujarat: GujaratImage,
+    Meghalaya: MeghalayaImage,
+    Assam: AssamImage,
+    Nagaland: NagalandImage,
+    Manipur: ManipurImage,
+    // Mizoram: Miz
+    // Tripura:
+    ArunachalPradesh: APImage,
   };
 
   const handleClick = (stateName) => {
@@ -64,7 +106,7 @@ function App() {
             </div>
           </div>
         </div>
-       
+
         <div className="India-map">
           <div className="inputs">
             <Input />
